e2beaa3d07d4363d6c356cd3f776f497534632c7
Asad Ullah <f200213@cfd.nu.edu.pk>
2022-12-04 01:13:31 +0500

    asad

--- Server/Routes/router.js
+++ Server/Routes/router.js
@@ -23,28 +23,38 @@ route.use(bodyParser.json());
 route.get('/forgot-password', (req, res) => {
 	res.render('forgotpass');
 })
+
 route.get('/about', (req, res) => {
 	res.render('about');
 })
 
+route.get('/home', (req, res) => {
+	res.render('home');
+})
+
+route.get('/dashboard', (req, res) => {
+	res.render('dashboard');
+})
 
 route.get('/', (req, res) => {
 	res.render('home');
 })
 
 route.get('/login', (req, res) => {
-	
-   
-	res.render('login',{msg:""});
+	res.render('login');
 })
+
 route.get('/signup', (req, res) => {
 	
-    res.render('signup',{msg:""});
+    res.render('signup');
 })
+
 route.get('/contact', (req, res) => {
 	
-    res.render('contact_us',{msg:""});
+    res.render('contact_us');
 })
+
+
 route.get('/userview', (req, res) => {
 	
 	const dataCountQuery = "SELECT COUNT(*) FROM users";
@@ -82,50 +92,25 @@ route.post('/', function(request, response) {
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		mysqlConnection.query('SELECT * FROM user2 WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		mysqlConnection.query('SELECT * FROM easysplit_users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
-				response.redirect('/');
+				response.redirect('/dashboard');
 			} else {
-				response.render('login',{msg:"Incorrect Username and/or Password!",success:false})
+				response.render('login',{success:false})
 			
 
 			}			
 			response.end();
 		});
 	} else {
-				response.render('login',{msg:'Please enter Username and Password!',success:false})
+				response.render('login',{success:false})
 		response.end();
 	}
 });
-route.post('/', function(request, response) {
-	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		mysqlConnection.query('SELECT * FROM user2 WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				response.redirect('/');
-			} else {
-				response.render('login',{msg:"Incorrect Username and/or Password!",success:false})
-				// response.redirect('/');
 
-				// response.send('Incorrect Username and/or Password!');
 
-			}			
-			response.end();
-		});
-	} else {
-				response.render('login',{msg:'Please enter Username and Password!',success:false})
-		response.end();
-	}
-});
 
 module.exports = route;
