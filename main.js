5ef10bff62213426b51d4c9910a56116ffce4d94
ghazih808 <116555180+ghazih808@users.noreply.github.com>
2022-12-08 04:30:18 +0500

    All done

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ app.set("view engine","ejs")
 
 //load assets
 // app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-// app.use('/Img',express.static(path.resolve(__dirname,"assets/Img")))
+app.use('/Img',express.static(path.resolve(__dirname,"assets/Img")))
 // app.use('/JS',express.static(path.resolve(__dirname,"assets/JS")))
- app.use(express.static(path.join(__dirname,"Assests")))
+//  app.use(express.static(path.join(__dirname,"Assests")))
 //load routers
 app.use('/',require('./Server/Routes/router'))
 
